584a8c5c155b2f24610425f4d768f305a737b26e
yashk <yashk7oo@gmail.com>
2020-12-07 12:47:17 +0530

    Demo commit

---
+++ Quick Start/src/com/training/Application.java
@@ -0,0 +1,17 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting welcome=new Greeting();
+		  
+		  String message=welcome.getMessage();
+		  System.out.println(message);
+
+		 }		 
+
+
+	}
+
+
